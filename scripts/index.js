3ce92b2b5572849a991e30ab0f39205c8b3dba3c
Alexander <miloslavskiy.sanek@yandex.ru>
2024-03-21 21:54:49 +0300

    Реализовано добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,46 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const template = document.querySelector("#card-template");
+const places = document.querySelector(".places__list");
+
+function createCard(data, deleteCard) {
+  const newCard = template.content.querySelector(".card").cloneNode(true);
+
+  newCard.querySelector(".card__image").src = data.link;
+  newCard.querySelector(".card__title").textContent = data.name;
+
+  return newCard;
+}
+
+function deleteCard() {}
+
+initialCards.forEach(function (element) {
+  const card = createCard(element, deleteCard);
+  places.append(card);
+});
+
+/*
+
+Задачи: 
+1. Рендерить карточки
+2. Удалять карточки
+
+
+1.1 Функция создания
+1.3 Отрендерить карточку
+
+Фунция создания:
+  Принимает данные
+  Создать карточку 
+  Записать данные в карточку
+  Добавить слушатель события
+  Вернуть карточку
+
+Рендер карточки:
+  Проитерироваться по массиву карточек 
+  Создаешь карточку
+  Добавляешь карточку
+  
+*/
